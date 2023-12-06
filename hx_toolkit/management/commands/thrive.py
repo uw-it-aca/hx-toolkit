bf20d3c8b95452ba98ecdf21c2e7473980a35a8b
Fang Lin <fanglin@uw.edu>
2023-12-05 16:57:04 -0800

    Add copyright

--- hx_toolkit/management/commands/thrive.py
+++ hx_toolkit/management/commands/thrive.py
@@ -1,3 +1,6 @@
+# Copyright 2023 UW-IT, University of Washington
+# SPDX-License-Identifier: Apache-2.0
+
 from django.core.management.base import BaseCommand, CommandError
 from hx_toolkit.db_dao import get_article_by_slug, create_article_data
 from django.conf import settings
